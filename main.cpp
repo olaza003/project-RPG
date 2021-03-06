557bb562dbd9635cd76f0837e420420bf16abac8
UrbanCoffee <rudy.m.7854@gmail.com>
2021-03-06 00:55:28 -0800

    Main.cpp and Output changes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
 #include "story.hpp"
 #include "sequenceManager/seqMan.hpp"
-//#include ""
+#include "items/protoITEMS/protoITEMS.hpp"
+#include "items/protoItems/protoENCHANT.hpp"
+
+seqMan dia;
+string userInput;
+
+void quick_intro(Character*);
+vector<Monster*> mobs; //fill with pre-generated Monsters
+vector<string> preFightDia;
 
 int main() {
 
+    Character player;
+    quick_intro(&player);
+
+    int floorCounter = 0;
+
+    while(floorCounter < 5){
+        dia.sequence(preFightDia.at(floorCounter));
+
+        bool check = battle(&player, mobs.at(floorCounter));
+
+    }
+    
+
     return 0;
+}
+
+string whichSequence(int val)
+{
+    if(val == 0) return "[SEQ_ONE]";
+    else if(val == 1) return "[SEQ_TWO]";
+    else if(val == 2) return "[SEQ_THREE]";
+    else if(val == 3) return "[SEQ_FOUR]";
+}
+
+void quick_intro(Character* player){
+    dia.file("Dialogue/Introduction.txt"); dia.sequence("[INTRODUCTION]");
+    dia.OUT();
+    //cin >> userInput; player.setName(userInput);
+    dia.OUT();
+    CharacterCreation(player);
+    dia.OUT();
+    //output list of weapons
+    //cin >> item choices
+    dia.OUT();
+    //player's choice outputs
 }
--- story.hpp
+++ story.hpp
@@ -15,15 +15,15 @@ char battleOp;
 void CharacterCreation(Character* p)
 {
     char menuOp;
-    cout << "Which character do you want to make?" << endl;
+    cout << ">>These are the classes you can choose from:" << endl;
     
-    cout << "Warriors specialize in dealing damage which will help with attacking" << endl;
-    cout << "Knights specialize in defense which will help with defending" << endl;
-    cout << "Hunters specialize in speed whihc will help with dodging" << endl;
+    cout << "  || Warriors specialize in dealing damage which will help with attacking" << endl;
+    cout << "  || Knights specialize in defense which will help with defending" << endl;
+    cout << "  || Hunters specialize in speed which will help with dodging" << endl;
     
-    cout << "Type 'W' for Warrior" << endl;
-    cout << "Type 'K' for Knight" << endl;
-    cout << "Type 'H' for Hunter" << endl;
+    cout << ">>Type 'W' for Warrior" << endl;
+    cout << ">>Type 'K' for Knight" << endl;
+    cout << ">>Type 'H' for Hunter" << endl;
     
     do
     {
@@ -68,15 +68,15 @@ void heal(Character* p)
 {
     if (p -> getHeals() == 0)
     {
-        cout << "OUT OF HEALS" << endl;
+        cout << ">>OUT OF HEALS!" << endl;
         battleOp = ' ';
         return;
     }
     
     p -> setHealth(-20);
     p -> setHeals(p -> getHeals() - 1);
-    cout << "HEALED FOR: 20" << endl;
-    cout << "HEALS REMAINING: " << p -> getHeals() << endl;
+    cout << ">>HEALED FOR: 20" << endl;
+    cout << ">>HEALS REMAINING: " << p -> getHeals() << endl;
 }
 
 void enemyAction(Character* p, Monster* e)
@@ -84,7 +84,7 @@ void enemyAction(Character* p, Monster* e)
     int rng = rand() % 10 + 1;
     if (rng >= 7)
     {
-        cout << "ENEMY ATTACKED" << endl;
+        cout << ">>ENEMY ATTACKED!" << endl;
         e -> attack(p);
     }
     else
@@ -96,22 +96,22 @@ void enemyAction(Character* p, Monster* e)
 
 bool battle(Character* player, Monster* enemy)
 {
-    cout << "An enemy has appeared!" << endl;
+    cout << ">>An enemy has appeared!" << endl;
     
     while (player -> getHealth() > 0 && enemy -> getHealth() > 0)
     {
         do
         {
             cout << endl;
-            cout << "Player Health: " << player -> getHealth();
-            cout << "   Enemy Health: " << enemy -> getHealth() << endl;
+            cout << ">>Player Health: " << player -> getHealth();
+            cout << "     Enemy Health: " << enemy -> getHealth() << endl;
             
-            cout << "What do you want to do?" << endl;
-            cout << "Type 1 to attack" << endl;
-            cout << "Type 2 to defend" << endl;
-            cout << "Type 3 to dodge" << endl;
-            //cout << "Type 4 to check inventory" << endl;
-            cout << "Type 5 to heal" << endl;
+            cout << ">>What do you want to do?" << endl;
+            cout << "  || Type 1 to attack" << endl;
+            cout << "  || Type 2 to defend" << endl;
+            cout << "  || Type 3 to dodge" << endl;
+            //cout << "  || Type 4 to check inventory" << endl;
+            cout << "  || Type 5 to heal" << endl;
             
             cin >> battleOp;
             
@@ -120,30 +120,30 @@ bool battle(Character* player, Monster* enemy)
                 case '1' :
                     player -> setDefend(false);
                     player -> setDodge(false);
-                    cout << "PLAYER ATTACKED" << endl;
+                    cout << ">>PLAYER ATTACKED!" << endl;
                     player -> attack(enemy);
                     break;
                 case '2' :
                     player -> setDodge(false);
-                    cout << "PLAYER DEFENDED" << endl;
+                    cout << ">>PLAYER DEFENDED!" << endl;
                     defend(player);
                     break;
                 case '3' :
                     player -> setDefend(false);
-                    cout << "PLAYER TRIED TO DODGE" << endl;
+                    cout << ">>PLAYER TRIED TO DODGE!" << endl;
                     dodge(player);
                     break;
                 //case 4 :
-                    //cout << "CHECKING INVENTORY" << endl;
+                    //cout << ">>CHECKING INVENTORY!" << endl;
                     //player -> getInventory();
                     //break;
                 case '5' :
                     player -> setDefend(false);
                     player -> setDodge(false);
-                    cout << "PLAYER HEALED" << endl;
+                    cout << ">>PLAYER HEALED!" << endl;
                     heal(player);
                     break;
-                default : cout << "ERROR, INVALID OPTION" << endl;
+                default : cout << ">>ERROR, INVALID OPTION" << endl;
             }
         } while (battleOp != '1' && battleOp != '2' && battleOp != '3' && battleOp != '5');
         if (enemy -> getHealth() > 0)
@@ -162,7 +162,7 @@ void gameOver(Character* p, Monster* e)
 {
     if (battle(p, e) == true)
     {
-        cout << "GAME OVER";
+        cout << "GAME OVER" << endl;
         //Report(p);
         exit(0);
     }
